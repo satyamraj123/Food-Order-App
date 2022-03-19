6826a7ae795a163661d7144a598363d63558f945
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-19 16:04:31 +0530

    header

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import react from "react";
+import styles from './HeaderCartButton.module.css';
+import CartIcon from "../Cart/CartIcon";
+const HeaderCartButton=(props)=>{
+return <button className={styles.button}>
+    <span className={styles.icon}>
+        <CartIcon />
+    </span>
+    <span>Your Cart</span>
+    <span className={styles.badge}>3</span>
+</button>
+}
+
+export default HeaderCartButton;
