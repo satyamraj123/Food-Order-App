41686d810cb118f6423ed355b688a1909282bb47
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-21 00:38:14 +0530

    bump effect on cart icon

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,29 @@
-import react, { useContext } from "react";
+import react, { useContext, useEffect,useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
+  const [buttonIsBump, setButtonIsBump] = useState(false);
   const ctx = useContext(CartContext);
-  const numberofCartitems = ctx.items.reduce((curr, item) => {
+  const items=ctx.items;
+  const numberofCartitems = items.reduce((curr, item) => {
     return curr + item.amount;
   }, 0);
+  const buttonclasses = `${styles.button} ${buttonIsBump?styles.bump : ''}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsBump(true);
+    const timer = setTimeout(() => {
+      setButtonIsBump(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonclasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
